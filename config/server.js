f23fe8ea9c424673a2889d5d8249996203286f8c
lelopereira05 <lelopereira05@hotmail.com>
2018-01-30 10:43:27 -0200

    alteração server js

--- config/server.js
+++ config/server.js
@@ -1,30 +1,18 @@
 var express = require('express');
-
 var consign = require('consign');
-
 var bodyParser = require('body-parser');
-
 var expressValidator = require('express-validator');
-
 var expressSession = require('express-session');
-
+var rootfile = require ( './bin/www' ); 
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views')
 
-
-// index page 
-app.get('/', function(req, res) {
-  res.render('home/index');
-});
-
 app.use(express.static('./app/public'));
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use(expressValidator());
 
 /**Configura o middleware express-session */
