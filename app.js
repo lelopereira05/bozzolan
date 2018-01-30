1a0df3ee28e47e20ee3800b215c0789b7b322ee9
lelopereira05 <lelopereira05@hotmail.com>
2018-01-30 10:35:48 -0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var app = require('./config/server');
 
-app.listen(3000, function(){
-	console.log('Servidor ON');
-});
+// app.listen(3000, function(){
+// 	console.log('Servidor ON');
+// });
 
-// var http = require('http')
-// var port = process.env.PORT || 1337;
-// http.createServer(function(req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     app.get('/', function(req, res) {
-//       res.render('home/index');
-//     });
-// }).listen(port);
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
 // index page 
